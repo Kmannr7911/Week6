b7545ce200564e630c6463745c7520c268ec5861
ConnorK26 <cklingmnn5197@gmail.com>
2022-03-09 13:00:29 -0500

    Update Week6.js

--- Week6.js
+++ Week6.js
@@ -1,3 +1,5 @@
+//Connor Klingman
+//
 // For the final project you will be creating an automated version of the classic card game WAR.
 // Think about how you would build this project and write your plan down. Consider classes such as Card, Deck, and Player and what fields and methods they might each have. You can implement the game however you’d like (i.e. printing to the console, using alert, or some other way). The completed project should, when ran, do the following:
 // Deal 26 Cards to two Players from a Deck. 
@@ -6,7 +8,7 @@
 // Ties result in zero points for either Player
 // After all cards have been played, display the score.
 // Write a Unit Test using Mocha and Chai for at least one of the functions you write.
-
+//
 
 var addArray = function (fromArray,toArray){
     var fromArrayIndex = 0;
